95b9ba210f018da2be6e1f8b1a1db556c2f29863
Ankit Kashyap <ankitwww@gmail.com>
2020-07-03 20:34:36 -0700

    Initial commit

--- handlers/candidates/screening.js
+++ handlers/candidates/screening.js
@@ -2,53 +2,53 @@
 const qs = require("querystring");
 const mongoose = require("mongoose");
 require("dotenv").config();
-var db = require("../../db");
-var JobApplication = db.jobApplication;
+let db = null;
+let conn = null;
+let JobApplication = null;
 
-mongoose.connect(process.env.MONGOCONN, {
-  useMongoClient: true,
-  bufferCommands: false, // Disable mongoose buffering
-  bufferMaxEntries: 0, // and MongoDB driver buffering
-});
-
-mongoose.connection.on("connected", function (ref) {
-  console.log("Connected to mongo server.");
-});
-
-mongoose.connection.on("error", function (err) {
-  console.log("Could not connect to mongo server!", err);
-});
-
-module.exports.notCompleted = (event, context, callback) => {
+module.exports.notCompleted = async (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
   var startDate = new Date();
   var startTime = startDate.getTime();
   var oneDay = 24 * 60 * 60 * 1000;
   var endTime = startTime + oneDay;
+
   try {
-    MongoClient.connect(process.env.MONGOCONN, function (err, client) {
-      if (!err && client) {
-        JobApplication.find(
-          {
-            appliedDate: { $gte: startTime, $lt: endTime },
-            status: "Yettoscreen",
-          },
-          { candidateEmail: 1 }
-        ).exec(function (jErr, apps) {
-          if (!jErr) {
-            console.log("Found ", apps.length);
-            callback(null, { apps });
-          } else {
-            console.log(jErr);
-            callback(jErr, { message: "something went wrong!" });
-          }
-        });
+    if (conn == null) {
+      conn = mongoose.createConnection(process.env.MONGOCONN, {
+        //useMongoClient: true,
+        bufferCommands: false, // Disable mongoose buffering
+        bufferMaxEntries: 0, // and MongoDB driver buffering
+        // reconnectTries: 30,
+        // reconnectInterval: 500,
+        // poolSize: 1,
+        // socketTimeoutMS: 2000000,
+        // keepAlive: true,
+      });
+      await conn;
+      db = require("../../db");
+      JobApplication = db.jobApplication;
+    }
+    console.log(
+      `{appliedDate: { $gte: ${startTime}, $lt: ${endTime} },status: 'Yettoscreen',}`
+    );
+    JobApplication.find(
+      {
+        appliedDate: { $gte: startTime, $lt: endTime },
+        status: "Yettoscreen",
+      },
+      { candidateEmail: 1 }
+    ).exec(function (jErr, apps) {
+      if (!jErr) {
+        console.log("Found ", apps.length);
+        callback(null, { apps });
       } else {
-        console.log(err);
-        callback(err, { message: "something went wrong!" });
+        console.log(jErr);
+        callback(jErr, { message: "something went wrong!" });
       }
     });
   } catch (jerr) {
-    console.error("an error occurred", err);
-    callback(jErr, { message: "something went wrong!" });
+    console.error("an error occurred", jerr);
+    callback(jerr, { message: "something went wrong!" });
   }
 };
